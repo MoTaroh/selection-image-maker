827dd21dff88966b5a31ea874113ef00e9506686
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-10-27 14:46:30 +0000

    add preview base

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import html2canvas from 'html2canvas'
+// import html2canvas from 'html2canvas'
 import React from 'react'
+import {ArrowRightIcon} from '@heroicons/react/outline'
 
 const App = () => {
     // const clickev = () => {
@@ -12,23 +13,23 @@ const App = () => {
     return (
         <div className="flex w-screen h-screen">
             <div className="flex flex-col">
-                <header className="px-8 pb-6 space-y-5 bg-white shadow">
+                <header className="px-8 pb-6 space-y-5 bg-white border-b border-gray-200">
                     <ul className="flex justify-between w-full py-6">
                         <li className="flex flex-col items-center justify-center space-y-4">
-                        <div className="flex items-center justify-center w-8 h-8 text-lg font-bold text-white bg-green-400 rounded-full">1</div>
-                        <div className="font-bold text-gray-700">テンプレート</div>
+                            <div className="flex items-center justify-center w-8 h-8 text-lg font-bold text-white bg-green-400 rounded-full">1</div>
+                            <div className="font-bold text-gray-700">テンプレート</div>
                         </li>
                         <li className="flex flex-col items-center justify-center space-y-4">
-                        <div className="flex items-center justify-center w-8 h-8 text-lg font-bold text-gray-300 bg-white border-2 border-gray-300 rounded-full">2</div>
-                        <div className="font-bold text-gray-300">お題</div>
+                            <div className="flex items-center justify-center w-8 h-8 text-lg font-bold text-gray-300 bg-white border-2 border-gray-300 rounded-full">2</div>
+                            <div className="font-bold text-gray-300">お題</div>
                         </li>
                         <li className="flex flex-col items-center justify-center space-y-4">
-                        <div className="flex items-center justify-center w-8 h-8 text-lg font-bold text-gray-300 bg-white border-2 border-gray-300 rounded-full">3</div>
-                        <div className="font-bold text-gray-300">コンテンツ</div>
+                            <div className="flex items-center justify-center w-8 h-8 text-lg font-bold text-gray-300 bg-white border-2 border-gray-300 rounded-full">3</div>
+                            <div className="font-bold text-gray-300">コンテンツ</div>
                         </li>
                         <li className="flex flex-col items-center justify-center space-y-4">
-                        <div className="flex items-center justify-center w-8 h-8 text-lg font-bold text-gray-300 bg-white border-2 border-gray-300 rounded-full">4</div>
-                        <div className="font-bold text-gray-300">シェア</div>
+                            <div className="flex items-center justify-center w-8 h-8 text-lg font-bold text-gray-300 bg-white border-2 border-gray-300 rounded-full">4</div>
+                            <div className="font-bold text-gray-300">シェア</div>
                         </li>
                     </ul>
                     <div className="flex">
@@ -36,28 +37,28 @@ const App = () => {
                         <div className="ml-4 text-lg font-bold text-gray-700">まずはテンプレートを選びましょう！</div>
                     </div>
                 </header>
-                <body className="grid flex-1 max-w-2xl grid-cols-3 gap-8 px-8 py-4 overflow-y-scroll bg-gray-50">
-                    <div className="w-40 h-40 bg-gray-400"></div>
-                    <div className="w-40 h-40 bg-gray-400"></div>
-                    <div className="w-40 h-40 bg-gray-400"></div>
-                    <div className="w-40 h-40 bg-gray-400"></div>
-                    <div className="w-40 h-40 bg-gray-400"></div>
-                    <div className="w-40 h-40 bg-gray-400"></div>
-                    <div className="w-40 h-40 bg-gray-400"></div>
-                    <div className="w-40 h-40 bg-gray-400"></div>
+                <body className="grid flex-1 max-w-2xl grid-cols-3 gap-8 px-8 py-4 overflow-y-scroll bg-gray-100">
+                    <div className="w-40 h-40 bg-gray-400 border-2 hover:border-green-400"></div>
+                    <div className="w-40 h-40 bg-gray-400 border-2 hover:border-green-400"></div>
+                    <div className="w-40 h-40 bg-gray-400 border-2 hover:border-green-400"></div>
+                    <div className="w-40 h-40 bg-gray-400 border-2 hover:border-green-400"></div>
+                    <div className="w-40 h-40 bg-gray-400 border-2 hover:border-green-400"></div>
+                    <div className="w-40 h-40 bg-gray-400 border-2 hover:border-green-400"></div>
+                    <div className="w-40 h-40 bg-gray-400 border-2 hover:border-green-400"></div>
+                    <div className="w-40 h-40 bg-gray-400 border-2 hover:border-green-400"></div>
                 </body>
-                <footer className="flex items-center py-4 bg-white">
-                    <button className="flex items-center px-6 py-3 border border-green-400 rounded-full">
+                <footer className="flex items-center justify-end px-8 py-4 bg-white border-t border-gray-200">
+                    <button className="flex items-center justify-between w-64 px-6 py-3 border border-green-400 rounded-full hover:bg-green-50">
                         <div className="flex flex-col items-start">
                             <div className="text-xs font-semibold text-gray-400">STEP 2</div>
                             <div className="text-lg font-bold text-green-400">お題を選ぶ</div>
                         </div>
-                        <div className="text-green-400">→</div>
+                        <ArrowRightIcon className="w-6 h-6 text-green-400"/>
                     </button>
                 </footer>
             </div>
-            <div className="flex-1 bg-gray-100">
-                preview
+            <div className="flex flex-1 p-8 bg-gray-100 border-l border-gray-300 justify-items-center">
+                <div className="w-full h-full bg-gray-300"></div>
             </div>
         </div>
     )
