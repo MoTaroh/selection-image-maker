7f8bce5d226e4dfde0f9e140a5da99c33fbaf5fa
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-11-23 08:37:38 +0000

    Add: context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 // import html2canvas from 'html2canvas'
-import React from 'react'
+import React, { useState, createContext } from 'react'
 import Step1 from './pages/Step1'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Step2 from './pages/Step2'
 import Step3 from './pages/Step3'
 import Preview from './components/Preview'
+
+
+type InitialState = {
+    currentStep: number;
+    setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
+};
+export const SelectionContext = createContext<InitialState>({
+    currentStep: 2,
+    setCurrentStep: () => {},
+});
 
 const App = () => {
     // const clickev = () => {
@@ -15,17 +25,22 @@ const App = () => {
     //         document.body.appendChild(canvas)
     //     });
     // }
+    const [currentStep, setCurrentStep] = useState<number>(2);
     return (
-        <div className="flex w-screen h-screen">
-            <div className="flex flex-col w-710px">
-                <Header step={1} ></Header>
-                <Step3></Step3>
-                <Footer></Footer>
+        <SelectionContext.Provider value={{currentStep, setCurrentStep}} >
+            <div className="flex w-screen h-screen">
+                <div className="flex flex-col w-710px">
+                    <Header></Header>
+                    {currentStep === 0 && (<Step1></Step1>)}
+                    {currentStep === 1 && (<Step2></Step2>)}
+                    {currentStep === 2 && (<Step3></Step3>)}
+                    <Footer></Footer>
+                </div>
+                <div className="flex flex-1 p-8 bg-gray-100 border-l border-gray-300 justify-items-center">
+                    <Preview></Preview>
+                </div>
             </div>
-            <div className="flex flex-1 p-8 bg-gray-100 border-l border-gray-300 justify-items-center">
-                <Preview></Preview>
-            </div>
-        </div>
+        </SelectionContext.Provider>
     )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Step, Stepper } from 'react-form-stepper';
+import {SelectionContext} from '../App'
 
-type Step = 1 | 2 | 3 | 4;
-type Props = {
-    step: Step
-}
+type StepType = 0 | 1 | 2 | 3;
 
 const ConnectorStyleProps = {
     disabledColor: "#CBD5E1",
@@ -29,7 +27,12 @@ const StepStyleDTO = {
     fontWeight: 700,
 }
 
-const Header = (props: Props) => {
+const Header: React.VFC = () => {
+    const steps = useContext(SelectionContext)
+    const handleCurrentStep = (value: StepType) => {
+        console.log(value)
+        steps.setCurrentStep(value);
+    }
     const titleMessage = [
         "まずはテンプレートを選びましょう！",
         "今回のお題は何にしましょう？",
@@ -39,15 +42,15 @@ const Header = (props: Props) => {
 
     return (
         <div className="px-8 pb-6 space-y-5 bg-white border-b border-gray-200">
-            <Stepper activeStep={1} connectorStateColors connectorStyleConfig={ConnectorStyleProps} styleConfig={StepStyleDTO} className="w-full text-gray-700">
-                <Step label="テンプレート" />
-                <Step label="お題" />
-                <Step label="コンテンツ" />
-                <Step label="シェア" />
+            <Stepper activeStep={steps.currentStep} connectorStateColors connectorStyleConfig={ConnectorStyleProps} styleConfig={StepStyleDTO} className="w-full text-gray-700">
+                <Step label="テンプレート" onClick={()=> handleCurrentStep(0)}/>
+                <Step label="お題" onClick={()=> handleCurrentStep(1)}/>
+                <Step label="コンテンツ" onClick={()=> handleCurrentStep(2)}/>
+                <Step label="シェア" onClick={()=> handleCurrentStep(3)}/>
             </Stepper>
             <div className="flex">
-                <div className="text-xs font-bold text-gray-400">STEP {props.step}</div>
-                <div className="ml-4 text-lg font-bold text-gray-700">{titleMessage[props.step-1]}</div>
+                <div className="text-xs font-bold text-gray-400">STEP {steps.currentStep+1}</div>
+                <div className="ml-4 text-lg font-bold text-gray-700">{titleMessage[steps.currentStep]}</div>
             </div>
         </div>
     )
