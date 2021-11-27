b8a8c29fa54294f956deec6007ffeff23b44e13e
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-11-27 08:35:04 +0000

    Refactor: change indent

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,75 @@
 // import html2canvas from 'html2canvas'
-import React, { useState, createContext } from 'react'
-import Step1 from './pages/Step1'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Step2 from './pages/Step2'
-import Step3 from './pages/Step3'
-import Preview from './components/Preview'
-import Step4 from './pages/Step4'
+import React, { useState, createContext } from "react";
+import Step1 from "./pages/Step1";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Step2 from "./pages/Step2";
+import Step3 from "./pages/Step3";
+import Preview from "./components/Preview";
+import Step4 from "./pages/Step4";
 
 export type SelectionContent = {
-    title: string;
-    description: string;
-    photo: string; // URL or unicode ?
-}
+  title: string;
+  description: string;
+  photo: string; // URL or unicode ?
+};
 
 type InitialState = {
-    currentStep: number;
-    setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
-    selectionTitle: string;
-    setSelectionTitle: React.Dispatch<React.SetStateAction<string>>;
-    selectionContent: Array<SelectionContent>;
-    setSelectionContent: React.Dispatch<React.SetStateAction<SelectionContent[]>>;
+  currentStep: number;
+  setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
+  selectionTitle: string;
+  setSelectionTitle: React.Dispatch<React.SetStateAction<string>>;
+  selectionContent: Array<SelectionContent>;
+  setSelectionContent: React.Dispatch<React.SetStateAction<SelectionContent[]>>;
 };
 export const SelectionContext = createContext<InitialState>({
-    currentStep: 0,
-    setCurrentStep: () => {},
-    selectionTitle: "",
-    setSelectionTitle: () => {},
-    selectionContent: [{ title: "", description: "", photo: "" }],
-    setSelectionContent: () => {}
+  currentStep: 0,
+  setCurrentStep: () => {},
+  selectionTitle: "",
+  setSelectionTitle: () => {},
+  selectionContent: [{ title: "", description: "", photo: "" }],
+  setSelectionContent: () => {},
 });
 
-
 const App = () => {
-    // const clickev = () => {
-    //     const html: any = document.getElementById('toImage')
-    //     console.log(html)
-    //     html2canvas(html).then(function(canvas) {
-    //         document.body.appendChild(canvas)
-    //     });
-    // }
-    const [currentStep, setCurrentStep] = useState<number>(0);
-    const [selectionTitle, setSelectionTitle] = useState<string>("");
-    const [selectionContent, setSelectionContent] = useState<Array<SelectionContent>>(
-        [{ title: "test", description: "tes", photo: "te" }]
-    );
-    return (
-        <SelectionContext.Provider value={{currentStep, setCurrentStep, selectionTitle, setSelectionTitle, selectionContent, setSelectionContent}} >
-            <div className="flex w-screen h-screen">
-                <div className="flex flex-col w-710px">
-                    <Header></Header>
-                    {currentStep === 0 && (<Step1></Step1>)}
-                    {currentStep === 1 && (<Step2></Step2>)}
-                    {currentStep === 2 && (<Step3></Step3>)}
-                    {currentStep === 3 && (<Step4></Step4>)}
-                    <Footer></Footer>
-                </div>
-                <div className="flex flex-1 p-8 bg-gray-100 border-l border-gray-300 justify-items-center">
-                    <Preview></Preview>
-                </div>
-            </div>
-        </SelectionContext.Provider>
-    )
-}
+  // const clickev = () => {
+  //     const html: any = document.getElementById('toImage')
+  //     console.log(html)
+  //     html2canvas(html).then(function(canvas) {
+  //         document.body.appendChild(canvas)
+  //     });
+  // }
+  const [currentStep, setCurrentStep] = useState<number>(0);
+  const [selectionTitle, setSelectionTitle] = useState<string>("");
+  const [selectionContent, setSelectionContent] = useState<
+    Array<SelectionContent>
+  >([{ title: "test", description: "tes", photo: "te" }]);
+  return (
+    <SelectionContext.Provider
+      value={{
+        currentStep,
+        setCurrentStep,
+        selectionTitle,
+        setSelectionTitle,
+        selectionContent,
+        setSelectionContent,
+      }}
+    >
+      <div className="flex w-screen h-screen">
+        <div className="flex flex-col w-710px">
+          <Header></Header>
+          {currentStep === 0 && <Step1></Step1>}
+          {currentStep === 1 && <Step2></Step2>}
+          {currentStep === 2 && <Step3></Step3>}
+          {currentStep === 3 && <Step4></Step4>}
+          <Footer></Footer>
+        </div>
+        <div className="flex flex-1 p-8 bg-gray-100 border-l border-gray-300 justify-items-center">
+          <Preview></Preview>
+        </div>
+      </div>
+    </SelectionContext.Provider>
+  );
+};
 
-export default App
+export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,64 +1,74 @@
-import React, { useContext } from 'react'
-import { Step, Stepper } from 'react-form-stepper';
-import {SelectionContext} from '../App'
+import React, { useContext } from "react";
+import { Step, Stepper } from "react-form-stepper";
+import { SelectionContext } from "../App";
 
 type StepType = 0 | 1 | 2 | 3;
 
 const ConnectorStyleProps = {
-    disabledColor: "#CBD5E1",
-    activeColor: "#34D399",
-    completedColor: "#34D399",
-    size: 1,
-    stepSize: '2em',
-    style: 'solid'
-}
+  disabledColor: "#CBD5E1",
+  activeColor: "#34D399",
+  completedColor: "#34D399",
+  size: 1,
+  stepSize: "2em",
+  style: "solid",
+};
 
 const StepStyleDTO = {
-    activeBgColor: '#34D399',
-    activeTextColor: '#ffffff',
-    completedBgColor: '#34D399',
-    completedTextColor: '#ffffff',
-    inactiveBgColor: '#CBD5E1',
-    inactiveTextColor: '#ffffff',
-    size: '2em',
-    circleFontSize: '1.125rem',
-    labelFontSize: '1rem',
-    borderRadius: '50%',
-    fontWeight: 700,
-}
+  activeBgColor: "#34D399",
+  activeTextColor: "#ffffff",
+  completedBgColor: "#34D399",
+  completedTextColor: "#ffffff",
+  inactiveBgColor: "#CBD5E1",
+  inactiveTextColor: "#ffffff",
+  size: "2em",
+  circleFontSize: "1.125rem",
+  labelFontSize: "1rem",
+  borderRadius: "50%",
+  fontWeight: 700,
+};
 
 const Header: React.VFC = () => {
-    const selections = useContext(SelectionContext)
-    const handleCurrentStep = (value: StepType) => {
-        selections.setCurrentStep(value);
+  const selections = useContext(SelectionContext);
+  const handleCurrentStep = (value: StepType) => {
+    selections.setCurrentStep(value);
+  };
+  const titleMessage = () => {
+    switch (selections.currentStep) {
+      case 0:
+        return "まずはテンプレートを選びましょう！";
+      case 1:
+        return "今回のお題は何にしましょう？";
+      case 2:
+        const title = selections.selectionTitle;
+        return `${title}を教えてください！`;
+      case 3:
+        return "お疲れさまでした！みんなにシェアしましょう！";
     }
-    const titleMessage = () => {
-        switch (selections.currentStep) {
-            case 0:
-                return "まずはテンプレートを選びましょう！"
-            case 1:
-                return "今回のお題は何にしましょう？"
-            case 2:
-                const title = selections.selectionTitle;
-                return `${title}を教えてください！`
-            case 3:
-                return "お疲れさまでした！みんなにシェアしましょう！"
-        }
-    }
-    return (
-        <div className="px-8 pb-6 space-y-5 bg-white border-b border-gray-200">
-            <Stepper activeStep={selections.currentStep} connectorStateColors connectorStyleConfig={ConnectorStyleProps} styleConfig={StepStyleDTO} className="w-full text-gray-700">
-                <Step label="テンプレート" onClick={()=> handleCurrentStep(0)}/>
-                <Step label="お題" onClick={()=> handleCurrentStep(1)}/>
-                <Step label="コンテンツ" onClick={()=> handleCurrentStep(2)}/>
-                <Step label="シェア" onClick={()=> handleCurrentStep(3)}/>
-            </Stepper>
-            <div className="flex">
-                <div className="text-xs font-bold text-gray-400">STEP {selections.currentStep+1}</div>
-                <div className="ml-4 text-lg font-bold text-gray-700">{titleMessage()}</div>
-            </div>
+  };
+  return (
+    <div className="px-8 pb-6 space-y-5 bg-white border-b border-gray-200">
+      <Stepper
+        activeStep={selections.currentStep}
+        connectorStateColors
+        connectorStyleConfig={ConnectorStyleProps}
+        styleConfig={StepStyleDTO}
+        className="w-full text-gray-700"
+      >
+        <Step label="テンプレート" onClick={() => handleCurrentStep(0)} />
+        <Step label="お題" onClick={() => handleCurrentStep(1)} />
+        <Step label="コンテンツ" onClick={() => handleCurrentStep(2)} />
+        <Step label="シェア" onClick={() => handleCurrentStep(3)} />
+      </Stepper>
+      <div className="flex">
+        <div className="text-xs font-bold text-gray-400">
+          STEP {selections.currentStep + 1}
         </div>
-    )
-}
+        <div className="ml-4 text-lg font-bold text-gray-700">
+          {titleMessage()}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
